d436d29d16aa28a77c55b1cbf9923c3c560d842f
Jefferson Hernandez <jeffersonh44@gmail.com>
2022-01-29 18:23:49 -0500

    working version of minesweeper

--- src/main.py
+++ src/main.py
@@ -50,10 +50,12 @@ def crearMatriz(filas, columnas, valor=0):
 def crearTablero(filas, columnas):
     tablero_minas = crearMatriz(filas, columnas)
     tablero_mostrar = crearMatriz(filas, columnas, valor="-")
+    tablero_visitado = crearMatriz(filas, columnas, valor=False)
 
     tablero = {
         "minas": tablero_minas,
         "mostrar": tablero_mostrar,
+        "visitado": tablero_visitado,
         "filas": filas,
         "columnas": columnas
     }
@@ -64,6 +66,7 @@ def imprimirTablero(tablero, test=False):
     for elem in tablero["mostrar"]:
         print(elem)
     if test:
+        print("to debug")
         for elem in tablero["minas"]:
             print(elem)
 
@@ -76,9 +79,11 @@ def crearJuego(filas, columnas, total_minas=10):
 def obtenerEntero(mensaje):
     while True:
         try:
-            valor = int(input(mensaje))
-            return valor
-        except:
+            valor = input(mensaje)
+            if valor == "salir":
+                exit()
+            return int(valor)
+        except TypeError:
             print(f"Debes introducir un entero, intentalo de nuevo")
 
 def mostrarMinas(tablero):
@@ -93,27 +98,76 @@ def mostrarMinas(tablero):
                 tablero_mostrar[i][j] = "x"
 
 
-def realizarAccion(fila, columna, tablero):
+def mostrarElemento(fila, columna, tablero):
+    tablero_mostrar = tablero["mostrar"]
+    tablero_minas = tablero["minas"]
+    tablero_mostrar[fila][columna] = str(tablero_minas[fila][columna])
+
+def expandirAccion(fila, columna, tablero):
+    tablero_visitado = tablero["visitado"]
+    if tablero_visitado[fila][columna]:
+        return
+    
     tablero_minas = tablero["minas"]
     if tablero_minas[fila][columna] == -1:
+        return
+
+    tablero_mostrar = tablero["mostrar"]
+
+    tablero_visitado[fila][columna] = True
+    minas = tablero_minas[fila][columna]
+    tablero_mostrar[fila][columna] = str(minas)
+
+    if minas > 0:
+        return
+    else:
+        filas, columnas = tablero["filas"], tablero["columnas"]
+        vecinos = obtenerVecindad(fila, columna, filas, columnas)
+        for i, j in vecinos:
+            expandirAccion(i, j, tablero)
+
+def verificarJuego(tablero):
+    tablero_minas = tablero["minas"]
+    tablero_visitado = tablero["visitado"]
+    filas = tablero["filas"]
+    columnas = tablero["columnas"]
+
+    for i in range(filas):
+        for j in range(columnas):
+            if not tablero_visitado[i][j] and tablero_minas[i][j] != -1:
+                return 0 # keep playing
+    return 1 # you win
+
+def realizarAccion(fila, columna, tablero):
+    tablero_minas = tablero["minas"]
+    tablero_visitado = tablero["visitado"]
+
+    if tablero_minas[fila][columna] == -1:
         mostrarMinas(tablero)
-        return False
-    return True
+        return -1
+    if tablero_minas[fila][columna] > 0:
+        tablero_visitado[fila][columna] = True
+        mostrarElemento(fila, columna, tablero)
+    if tablero_minas[fila][columna] == 0:
+        expandirAccion(fila, columna, tablero)
+    status = verificarJuego(tablero)
+    return status
 
 def obtenerDatos():
-    fila = obtenerEntero("Introduce la fila:")
-    columna = obtenerEntero("Introduce la columna:")
+    fila = obtenerEntero("Introduce la fila: ")
+    columna = obtenerEntero("Introduce la columna: ")
     return (fila, columna)
 
 def run():
-    tablero = crearJuego(9, 9)
-    is_playing = True
-    while is_playing:
-        imprimirTablero(tablero, test=True)
+    minas = int(input("introduce el número de minas: "))
+    tablero = crearJuego(9, 9, minas)
+    is_playing = 0
+    while is_playing == 0:
+        imprimirTablero(tablero, test=False)
         fila, columna = obtenerDatos()
         is_playing = realizarAccion(fila, columna, tablero)
     imprimirTablero(tablero)
-    print("Perdiste!")
+    print("Perdiste" if is_playing == -1 else "Ganaste")
 
 
 
