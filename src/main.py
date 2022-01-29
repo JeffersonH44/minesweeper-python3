136f62a975e854b36cc63f7e62398ee831ed40ce
Jefferson Hernandez <jeffersonh44@gmail.com>
2022-01-29 15:21:44 -0500

    print table working

--- src/main.py
+++ src/main.py
@@ -1,5 +1,42 @@
+import random
+
+def ponerMinas(minas, tablero):
+    minas = tablero["minas"]
+    filas = tablero["filas"]
+    columnas = tablero["columnas"]
+
+    counter = 0 
+    while counter < minas:
+        rand_fila = random.randint(0, filas)
+        rand_col = random.randint(0, columnas)
+        if minas[rand_fila][rand_col] == 1:
+            continue
+
+        minas[rand_fila][rand_col] = 1
+        counter += 1
+
+
+def crearTablero(filas, columnas):
+    tablero_minas = [[0] * columnas for _ in range(filas)]
+    tablero_mostrar = [[0] * columnas for _ in range(filas)]
+
+    tablero = {
+        "minas": tablero_minas,
+        "mostrar": tablero_mostrar,
+        "filas": filas,
+        "columnas": columnas
+    }
+
+    return tablero
+
+def imprimirTablero(tablero):
+    for elem in tablero["minas"]:
+        print(elem)
+    
 def run():
-    print("hello world")
+    tablero = crearTablero(9, 9)
+    imprimirTablero(tablero)
+
 
 if __name__ == "__main__":
     run()
